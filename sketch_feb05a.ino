f3b97b0b5204d8003769b62b88db30f7770039a7
sindhusunkara <104422693+sindhusunkara@users.noreply.github.com>
2024-05-21 12:29:38 +0530

    Add files via upload

---
+++ sketch_feb05a.ino
@@ -0,0 +1,187 @@
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
+
+
+Adafruit_BMP280 bmp; // I2C
+#include "Credentials.h"
+#define smPin 34
+#define relayPin 13
+int smValue;
+int smLimit = 1500;
+int delayTime = 2000;
+String motorStatus = "OFF";
+void OLEDInit();
+void OLEDUpdate();
+void WiFiInit();
+void FirebaseInit();
+void FirebaseWrite();
+void FirebaseRead();
+String tag1 = "IOTLAB/Smart_Garden/Soil_Moisture";
+String tag2 = "IOTLAB/Smart_Garden/Motor_Status";
+
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+float tempC,tempF,atPressure,altitude,humidity;
+void setup() {
+  pinMode(relayPin,OUTPUT);
+  Serial.begin(115200);
+  Serial.println(F("BMP280 test"));
+
+  if (!bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID)) {
+      Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    while (1) delay(10);
+  }
+
+  /* Default settings from datasheet. */
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+
+ OLEDInit();
+ WiFiInit();
+ FirebaseInit();
+}
+
+void loop() {
+    printValues();
+    displayValues();
+FirebaseRead();  
+smValue = 4095 - analogRead(smPin);
+Serial.print("Soil Moisture = ");
+Serial.println(smValue);
+
+if (smValue < smLimit){
+digitalWrite(relayPin,HIGH);
+Serial.println("Motor turned ON");
+motorStatus = "ON";
+}
+else{
+digitalWrite(relayPin,LOW);
+Serial.println("Motor turned OFF");
+motorStatus = "OFF";
+}
+
+OLEDUpdate();
+FirebaseWrite();
+    delay(2000);
+}
+
+ void OLEDInit(){
+  display.init();
+  display.setFont(ArialMT_Plain_16);
+}
+
+void OLEDUpdate(){
+  
+ 
+  display.clear();
+  display.drawString(30,0,String(smValue));
+  display.drawString(30,30,String(motorStatus));
+  display.display();
+}
+
+void printValues(){
+ // float tempC,tempF,atPressure,altitude,humidity;
+  Serial.print("Temperature = ");
+  tempC = bmp.readTemperature();
+//Serial.print(bme.readTemperature());
+Serial.print(tempC);
+Serial.println(" deg C");
+Firebase.setFloat(firebaseData,"IOTLAB/Environment_Monitor/Temperature",tempC);
+
+Serial.print("Temperature = ");
+//Serial.print(1.8*bme.readTemperature()+32);
+Serial.print(1.8*tempC+32);
+Serial.println(" deg F");
+
+Serial.print("Pressure = ");
+atPressure=bmp.readPressure()/100.0F;
+//Serial.print(bme.readPressure()/100.0F);
+Serial.print(atPressure);
+Serial.println(" hPa");
+Firebase.setFloat(firebaseData,"IOTLAB/Environment_Monitor/Pressure",atPressure);
+
+Serial.print("Altitude = ");
+altitude=bmp.readAltitude(SEALEVELPRESSURE_HPA);
+//Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+Serial.print(altitude);
+Serial.println(" m");
+Firebase.setFloat(firebaseData,"IOTLAB/Environment_Monitor/Altitude",altitude);
+Serial.println();
+Firebase.setFloat(firebaseData,"IOTLAB/Environment_Monitor/Humidity",0.0);
+
+}
+
+void displayValues(){
+  display.clear();
+  
+  String myString = "";
+  
+  char buffer[6];
+  dtostrf(tempC,5,1,buffer);
+  myString.concat(buffer);
+  myString.concat(" C");
+  display.drawString(0,0,myString);
+
+  humidity = 0.0;
+  myString = "";
+  dtostrf(humidity,5,1,buffer);
+  myString.concat(buffer);
+  myString.concat(" %");
+  display.drawString(64,0,myString);
+
+ myString = "";
+  dtostrf(atPressure,5,1,buffer);
+  myString.concat(buffer);
+   display.drawString(0,30,myString);
+
+    myString = "";
+  dtostrf(altitude,5,1,buffer);
+  myString.concat(buffer);
+  myString.concat("m");
+   display.drawString(64,30,myString);
+
+
+  display.display();
+
+}
+void WiFiInit(){
+  pinMode(2,OUTPUT);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    digitalWrite(2,!digitalRead(2));
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+   
+}
+void FirebaseInit(){
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+}
+void FirebaseWrite(){
+  Firebase.setInt(firebaseData,tag1,smValue);
+  Firebase.setString(firebaseData,tag2,motorStatus);
+  
+}
+
+void FirebaseRead(){
+String smCloudFull;
+String smCloud;
+if(Firebase.getString(firebaseData,"IOTLAB/Smart_Garden/SM_Threshold",smCloudFull)){
+smCloud = smCloudFull.substring(2,smCloudFull.length()-2);
+smLimit = smCloud.toInt();
+Serial.println(smLimit);
+}
+}
---
+++ soil_moisture.ino
@@ -0,0 +1,29 @@
+#include<Wire.h>
+#define smPin 34
+#define relayPin 13
+int smValue;
+int smLimit = 1500;
+int delayTime = 2000;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(relayPin,OUTPUT);
+  Serial.begin(115200);
+  }
+
+void loop() {
+  // put your main code here, to run repeatedly:
+smValue = 4095 - analogRead(smPin);
+Serial.print("Soil Moisture = ");
+Serial.println(smValue);
+
+if (smValue < smLimit){
+digitalWrite(relayPin,HIGH);
+Serial.println("Motor turned ON");
+}
+else{
+digitalWrite(relayPin,LOW);
+Serial.println("Motor turned OFF");
+}
+
+delay(delayTime);
+}
